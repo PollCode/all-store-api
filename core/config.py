28b0a8b34a109c1abfbb07fac5b9aaa4bb358efb
PollCode <pollyasiel@gmail.com>
2026-08-26 21:14:11 -0400

    feat: Config file created

---
+++ core/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    DB_HOST: str = os.getenv('DB_HOST')
+    DB_PORT: int = int(os.getenv('DB_PORT'))
+    DB_USER: str = os.getenv('DB_USER')
+    DB_PASSWORD: str = os.getenv('DB_PASSWORD')
+    DB_NAME: str = os.getenv('DB_NAME')
+    SECRET_KEY: str = os.getenv('SECRET_KEY')
+    ALGORITHM: str = os.getenv('ALGORITHM')
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
+    
+    class Config:
+        env_file = ".env"
