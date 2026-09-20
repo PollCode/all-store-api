95e837ef20ecb1f3fcddf56579054d7e83582945
PollCode <pollyasiel@gmail.com>
2026-09-20 14:26:48 -0400

    feat(users): add User model

--- common/models.py
+++ common/models.py
@@ -8,5 +8,5 @@ class BaseModel(SQLModel):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     created_at: datetime = Field(default_factory=lambda:datetime.now()) 
     created_by: str | None = Field(default=None)
-    updated_at: datetime = Field(default_factory=lambda: datetime.now(),sa_column_kwargs={"onupdate": func.now()})
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(), sa_column_kwargs={"onupdate": func.now()})
     updated_by: str | None = Field(default=None)
---
+++ models/users.py
@@ -0,0 +1,13 @@
+from sqlmodel import Field
+from common.models import BaseModel
+
+
+class User(BaseModel):
+    __tablename__ = "users"
+    
+    email: str = Field(index=True, unique=True)
+    password_hash: str = Field()
+    full_name: str = Field()
+    role: str
+    is_active: bool = Field(default=True)
+        
